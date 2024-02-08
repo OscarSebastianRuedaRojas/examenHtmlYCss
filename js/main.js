2b0e249316c7209be1d22ad773fe6144a875cfcf
OscarSebastianRuedaRojas <705ruedaoscar@gmail.com>
2024-02-08 07:26:16 -0500

    feat: :lipstick: Diseño de cajas
    Se realizo el diseño de las cajas, cambiando tambien su estructura en html, ademas se crearon las bases js para la funcionalidad del boton.
    
    @CampusLands

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let boton = document.querySelector('.botonMenu')
+let aside = document.querySelector('aside')
+let section = document.querySelector('section')
+
+boton.addEventListener('click'), function(){
+    aside.classList.toggle('active')
+    section.classList.toggle('active')
+}
