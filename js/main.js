1b991f076b3b47a030c7ff2a7b1ecd9d72fe3865
OscarSebastianRuedaRojas <705ruedaoscar@gmail.com>
2024-02-08 07:50:40 -0500

    feat: :lipstick: Diseños Productos
    Se arreglaron los diseños de los productos, ademas de arreglas la funcionalidad del boton
    
    @CampusLands

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let boton = document.querySelector('.botonMenu')
 let aside = document.querySelector('aside')
 let section = document.querySelector('section')
 
-boton.addEventListener('click'), function(){
+boton.addEventListener('click', function(){
     aside.classList.toggle('active')
     section.classList.toggle('active')
-}
+})
